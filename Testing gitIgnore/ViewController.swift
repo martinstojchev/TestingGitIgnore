26ba077dc45e26107dfb0aa9d9eefeab748ef336
Martin <martinstojchev@hotmail.com>
2018-10-25 08:36:27 +0200

    Initial Commit

---
+++ Testing gitIgnore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Testing gitIgnore
+//
+//  Created by Martin on 10/25/18.
+//  Copyright © 2018 Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
